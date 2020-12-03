e782240df8dfe305521ea7991f914956868d292c
Sander Loman <sander.loman@student.rocva.nl>
2020-12-03 12:10:34 +0100

    taak 6 af

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,17 @@
+<?php 
+$voornaam = 'Sander';
+$achternaam = True;
+$leeftijd = 17;
+$kleurogen = 'GroenBruin';
+$kleurhaar = null;
+
+echo gettype($voornaam);
+echo '<br>';
+echo gettype($achternaam);
+echo '<br>';
+echo gettype($leeftijd);
+echo '<br>';
+echo gettype($kleurogen);
+echo '<br>';
+echo gettype($kleurhaar);
+?>
