8e3be6694892d717ddee73f440769bcaa6bfd608
Sander Loman <sander.loman@student.rocva.nl>
2020-12-04 11:16:15 +0100

    taak 3-4 af

---
+++ 3-Arrays/taak04 - Multidimensionaal/index.php
@@ -0,0 +1,10 @@
+<?php
+$abcGetallen = ['A','B','C','D','24','56','78','90'];
+print_r($abcGetallen);
+echo "<br>";
+echo $abcGetallen[0];
+echo $abcGetallen[5];
+echo "<br>";
+echo $abcGetallen[2];
+echo $abcGetallen[7];
+?>
