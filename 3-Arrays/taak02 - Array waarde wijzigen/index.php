d82598e27da5b90d5332639a135dc3f3e8cddbf1
Sander Loman <sander.loman@student.rocva.nl>
2020-12-04 10:00:04 +0100

    taak 3-3 af

---
+++ 3-Arrays/taak02 - Array waarde wijzigen/index.php
@@ -0,0 +1,19 @@
+<?php 
+$MyDogNames = ['Henk','Gijs','Klaas'];
+echo $MyDogNames[0];
+echo "<br>";
+echo $MyDogNames[1];
+echo "<br>";
+echo $MyDogNames[2];
+echo "<br>";
+
+echo "<br>";
+$MyDogNames = ['Henk','Gijs','Klaas'];
+$MyDogNames[1] = 'Hond';
+echo $MyDogNames[0];
+echo "<br>";
+echo $MyDogNames[1];
+echo "<br>";
+echo $MyDogNames[2];
+echo "<br>";
+?>
---
+++ 3-Arrays/taak03 - Array waarde toevoegen/index.php
@@ -0,0 +1,13 @@
+<?php 
+$MyHeroes = ['Spiderman','Iron man','Thor'];
+array_push($MyHeroes,'Batman');
+echo $MyHeroes[0];
+echo "<br>";
+echo $MyHeroes[1];
+echo "<br>";
+echo $MyHeroes[2];
+echo "<br>";
+echo $MyHeroes[3];
+echo "<br>";
+print_r($MyHeroes);
+?>
