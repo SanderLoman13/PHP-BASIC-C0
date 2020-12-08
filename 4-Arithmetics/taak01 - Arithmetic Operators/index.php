731a607845dcca2385bf294588ed74e29139c1ec
Sander Loman <sander.loman@student.rocva.nl>
2020-12-08 11:24:14 +0100

    taak 4-1 af

---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,11 @@
+<?php 
+$getal1 = 10;
+$getal2 = 10;
+echo $getal1 + $getal2;
+echo '<br>';
+echo $getal1 - $getal2;
+echo '<br>';
+echo $getal1 * $getal2;
+echo '<br>';
+echo $getal1 / $getal2;
+?>
