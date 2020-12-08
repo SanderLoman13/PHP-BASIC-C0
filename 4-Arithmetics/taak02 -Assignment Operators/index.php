6f5ad88c11b32d57eadef86c7171e701111f24fc
Sander Loman <sander.loman@student.rocva.nl>
2020-12-08 11:34:32 +0100

    taak 4-2 af

---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,18 @@
+<?php 
+$getal1 = 50;
+$getal1 += 10;
+echo $getal1;
+echo '<br>';
+
+$getal1 -= 10;
+echo $getal1;
+echo '<br>';
+
+$getal1 *= 10;
+echo $getal1;
+echo '<br>';
+
+$getal1 /= 10;
+echo $getal1;
+echo '<br>';
+?>
