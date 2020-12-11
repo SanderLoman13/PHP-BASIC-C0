860b561ac0eb0df103e1f658aba903e43425d205
Sander Loman <sander.loman@student.rocva.nl>
2020-12-11 10:50:26 +0100

    taak 5-2 af

---
+++ 5-Conditional_Statements/taak02 - if conditional/index.php
@@ -0,0 +1,31 @@
+<?php 
+$test1 = 10;
+$test2 =  6;
+if($test1 == $test2){
+    echo "Gelijk";
+}
+echo "<br>";
+if($test1 <> $test2){
+    echo "Ongelijk";
+}
+echo "<br>";
+if($test1 === $test2){
+    echo "indentiek";
+}
+echo "<br>";
+if($test1 > $test2){
+    echo "Test 1 is groter dan test 2";
+}
+echo "<br>";
+if($test1 < $test2){
+    echo "Test 1 is kleiner dan test 2";
+}
+echo "<br>";
+if($test1 >= $test2){
+    echo "test1 is groter of gelijk aan test2";
+}
+echo "<br>";
+if($test1 <= $test2){
+    echo "test1 is kleiner of gelijk aan test2";
+}
+?>
