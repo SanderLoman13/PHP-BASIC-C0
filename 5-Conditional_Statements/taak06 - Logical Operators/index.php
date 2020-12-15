5cd64ffa0cbc49c0da1d77aecff91e10ffd281b9
Sander Loman <sander.loman@student.rocva.nl>
2020-12-15 13:56:23 +0100

    taak 5-6 af

---
+++ 5-Conditional_Statements/taak06 - Logical Operators/index.php
@@ -0,0 +1,27 @@
+<?php 
+$var1 = 6;
+
+if($var1 < 10 && $var1 > 5) {
+    echo "Het getal zit tussen de 10 en de 5!";
+} else {
+    echo "Het getal is groter dan 10 of kleiner dan 5!";
+}
+echo "<br>";
+if($var1 < 10 || $var1 > 5) {
+    echo "het getal is kleiner dan 5!";
+} else {
+    echo "Het getal is groter dan 10!";
+}
+echo "<br>";
+if($var1 < 10 xor $var1 > 5) {
+    echo "Het getal is 5 of lager";
+} else {
+    echo "Het getal zit tussen de 5 en 10";
+}
+echo "<br>";
+if($var1 < 10 ?? $var1 > 5) {
+    echo "Het getal is kleiner dan 10!";
+} else {
+    echo "Het getal is groter dan 10!";
+}
+?>
