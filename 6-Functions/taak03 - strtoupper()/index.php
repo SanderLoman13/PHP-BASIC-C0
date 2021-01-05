2a377ba1e7c740c38a1188d4820c99a6082da8b8
Sander Loman <sander.loman@student.rocva.nl>
2021-01-05 14:22:57 +0100

    opdracht 6-3 af

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,10 @@
+<?php 
+function telAlleWoorden(){
+    $heleZin = "Mijn blauwe auto is een mooie snelle wagen";
+    $heleZin;
+    strlen($heleZin);
+    $lengteZin = strlen($heleZin);
+    echo $lengteZin;
+}
+telAlleWoorden();
+?>
