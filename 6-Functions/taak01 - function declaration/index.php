9a93846b46801faa21cb746561f2d216556c310f
Sander Loman <sander.loman@student.rocva.nl>
2021-01-05 10:08:03 +0100

    Hulpvraag gepushed

---
+++ 6-Functions/taak01 - function declaration/index.php
@@ -0,0 +1,11 @@
+<?php
+function zetTweeWoordenAanElkaar(){
+    '$helezin';
+}
+
+$woordje = 'Hallo';
+$zinnetje = 'mooie blauwe planeet!';
+$helezin = [$woordje, $zinnetje];
+
+echo zetTweeWoordenAanElkaar();
+?>
