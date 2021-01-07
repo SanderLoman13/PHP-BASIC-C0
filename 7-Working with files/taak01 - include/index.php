73c37ea8b9c952abaa2f5c3c7803a0782283495c
Sander Loman <sander.loman@student.rocva.nl>
2021-01-07 18:29:21 +0100

    taak 7-1 af

---
+++ 7-Working with files/taak01 - include/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'C:\Users\sande\OneDrive - ROCvA, ROCvF en VOvA\jaar1\php-basic-c0\7-Working with files\taak01 - include\menu.php'?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
---
+++ 7-Working with files/taak01 - include/menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul><a href="menu2.php">Menu-item 1<a></ul>
+    <ul><a href="menu3.php">Menu-item 2<a></ul>
+    <ul><a href="menu4.php">Menu-item 3<a></ul>
+</body>
+</html>
