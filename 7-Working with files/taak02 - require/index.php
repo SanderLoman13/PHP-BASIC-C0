0929878c90fbe2aa27acb095257fe16b6f1f53cb
Sander Loman <sander.loman@student.rocva.nl>
2021-01-08 12:22:19 +0100

    taak 7-2 af

---
+++ 7-Working with files/taak02 - require/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require 'C:\Users\sande\OneDrive - ROCvA, ROCvF en VOvA\jaar1\php-basic-c0\7-Working with files\taak02 - require\includes\database.php'?>
+</head>
+<body>
+    <?php include 'C:\Users\sande\OneDrive - ROCvA, ROCvF en VOvA\jaar1\php-basic-c0\7-Working with files\taak02 - require\includes\menu.php'?>
+</body>
+</html>
